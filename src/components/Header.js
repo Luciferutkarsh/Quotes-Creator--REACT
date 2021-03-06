78de5cda00f0921b9f439e104ede7ed8f554d0a4
Luciferutkarsh <luciferutkarsh@gmail.com>
2021-03-06 23:42:47 +0530

    QUOTES FIRST COMMIT

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <React.Fragment>
+      <h2>Welcome to the React Quote Generator- LUCIFER UTKARSH</h2>
+      <p>
+        Built and styled by{' '}
+        <a href='https://www.linkedin.com/in/utkarsh-u-30874811b/' target='_none'>
+          UTKARSH UMANG
+        </a>
+      </p>
+    </React.Fragment>
+  );
+}
+
+export default Header;
